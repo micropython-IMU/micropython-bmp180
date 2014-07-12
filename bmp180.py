4573a43358a3d55e53226a69c1c36688a1311555
Sebastian Plamauer <oepse@gmail.com>
2014-07-12 20:41:42 +0200

    massive refactoring

--- bmp180.py
+++ bmp180.py
@@ -38,151 +38,143 @@ class BMP180():
     _bmp_addr = 119             # adress of BMP180 is hardcoded on the sensor
 
     # init
-    def __init__(self, side_char=None):
+    def __init__(self, side_str=None):
 
-        if side_char is None:
-            side = 2
-        elif str(side_char) in ('X', '1'):
-            side = 1
-        elif str(side_char) in ('Y', '2'):
-            side = 2
-        else:
+        # choose which i2c port to use
+        if side_str is None:
+            side_str = 'Y'
+        side_dict = {
+                        'X': 1,
+                        'Y': 2,
+                        '1': 1,
+                        '2': 2,     }
+        try:
+            side = side_dict[side_str]
+        except KeyError:
             print('pass either X, 1, Y, 2 or None, defaulting to Y')
             side = 2
 
-        self.oversample_sett = 0
+        # create i2c obect
         _bmp_addr = self._bmp_addr
-        self.bmp_i2c = pyb.I2C(side, pyb.I2C.MASTER)
-        self.chip_id = self.bmp_i2c.mem_read(2, _bmp_addr, 0xD0)
-        self.cal_AC1 = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xAA))[0]
-        self.cal_AC2 = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xAC))[0]
-        self.cal_AC3 = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xAE))[0]
-        self.cal_AC4 = unp('>H', self.bmp_i2c.mem_read(2, _bmp_addr, 0xB0))[0]
-        self.cal_AC5 = unp('>H', self.bmp_i2c.mem_read(2, _bmp_addr, 0xB2))[0]
-        self.cal_AC6 = unp('>H', self.bmp_i2c.mem_read(2, _bmp_addr, 0xB4))[0]
-        self.cal_B1 = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xB6))[0]
-        self.cal_B2 = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xB8))[0]
-        self.cal_MB = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xBA))[0]
-        self.cal_MC = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xBC))[0]
-        self.cal_MD = unp('>h', self.bmp_i2c.mem_read(2, _bmp_addr, 0xBE))[0]
-        self.pressure_MSL = 101325
+        self._bmp_i2c = pyb.I2C(side, pyb.I2C.MASTER)
+        self.chip_id = self._bmp_i2c.mem_read(2, _bmp_addr, 0xD0)
+        # read calibration data from EEPROM
+        self._AC1 = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xAA))[0]
+        self._AC2 = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xAC))[0]
+        self._AC3 = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xAE))[0]
+        self._AC4 = unp('>H', self._bmp_i2c.mem_read(2, _bmp_addr, 0xB0))[0]
+        self._AC5 = unp('>H', self._bmp_i2c.mem_read(2, _bmp_addr, 0xB2))[0]
+        self._AC6 = unp('>H', self._bmp_i2c.mem_read(2, _bmp_addr, 0xB4))[0]
+        self._B1 = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xB6))[0]
+        self._B2 = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xB8))[0]
+        self._MB = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xBA))[0]
+        self._MC = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xBC))[0]
+        self._MD = unp('>h', self._bmp_i2c.mem_read(2, _bmp_addr, 0xBE))[0]
 
-    # gauge uncompensated temperature
-    def gauge_uncomp_temperature(self):
+        # settings to be adjusted by user
+        self.oversample_sett = 0
+        self.temp_comp_sample_rate = 0.1
+        self.baseline = 101325
+
+        # priate attributes not to be used by user
+        self._t_temperature_ready = None
+        self._t_pressure_ready = None
+        self._B5 = None
+        self._t_B5 = None
+        self._dt_B5 = 1000/self.temp_comp_sample_rate
+
+    # gauge temperature
+    def gauge_temperature(self):
         '''
-        Starts the temperature measurement and returns the time it will be
+        Starts the temperature measurement and sets the time it will be
         finished.
         '''
-        self.bmp_i2c.mem_write(0x2E, self._bmp_addr, 0xF4)
-        return pyb.millis()+5
+        self._bmp_i2c.mem_write(0x2E, self._bmp_addr, 0xF4)
+        self._t_temperature_ready = pyb.millis()+5
+        return
 
-    # get uncompensated temperature
-    def get_uncomp_temperature(self, t_ready):
+    # get temperature
+    def get_temperature(self):
         '''
         Waits until the temperature measurement is finished, then returns the
-        uncompensated temperature.
+        temperature.
         '''
-        if pyb.millis() <= t_ready:
-            self.get_uncomp_temperature(t_ready)
+        if self._t_temperature_ready is None:
+            self.gauge_temperature()
+        elif pyb.millis() <= self._t_temperature_ready:
+            self.get_temperature()
         else:
-            return unp('>h', self.bmp_i2c.mem_read(2, self._bmp_addr, 0xF6))[0]
-
-    # uncompensated temperature
-    def uncomp_temperature(self):
-        '''
-        Measures and returns the uncompensated temperature.
-        '''
-        return self.get_uncomp_temperature(self.gauge_uncomp_temperature())
-
-    # gauge uncompensated pressure
-    def gauge_uncomp_pressure(self):
-        '''
-        Starts the pressure measurement and returns the time it will be
-        finished.
-        '''
-        delay = [5, 8, 14, 25]
-        self.bmp_i2c.mem_write(
-                                (0x34+(self.oversample_sett << 6)),
-                                self._bmp_addr,
-                                0xF4)
-
-        return pyb.millis() + pyb.delay(delay[self.oversample_sett])
-
-    # get uncompensated pressure
-    def get_uncomp_pressure(self, t_ready):
-        '''
-        Waits until the pressure measurement is finished, then returns the
-        uncompensated temperature.
-        '''
-        if pyb.millis() <= t_ready:
-            self.get_uncomp_pressure(t_ready)
-        else:
-            MSB = unp('<h', self.bmp_i2c.mem_read(1, self._bmp_addr, 0xF6))[0]
-            LSB = unp('<h', self.bmp_i2c.mem_read(1, self._bmp_addr, 0xF7))[0]
-            XLSB = unp('<h', self.bmp_i2c.mem_read(1, self._bmp_addr, 0xF8))[0]
-            return ((MSB << 16)+(LSB << 8)+XLSB) >> (8-self.oversample_sett)
-
-    # uncompensated pressure
-    def uncomp_pressure(self):
-        '''
-        Measures and returns the uncompensated pressure.
-        '''
-        return self.get_uncomp_pressure(self.gauge_uncomp_pressure())
+            UT = unp('>h', self._bmp_i2c.mem_read(2, self._bmp_addr, 0xF6))[0]
+            X1 = (UT-self._AC6)*self._AC5/2**15
+            X2 = self._MC*2**11/(X1+self._MD)
+            self._t_temperature_ready = None
+            return (((X1+X2)+8)/2**4)/10
 
     # B5 value for temperature compensation of pressure
-    def _B5(self, uncomp_temperature):
+    def _get_B5(self):
         '''
         Calculates and returns compensation value B5.
         '''
-        X1 = (uncomp_temperature-self.cal_AC6)*self.cal_AC5/2**15
-        X2 = self.cal_MC*2**11/(X1+self.cal_MD)
-        return X1+X2
+        if (self._B5 is None) or ((pyb.millis()-self._t_B5) > self._dt_B5):
+            X1 = (self.get_temperature()-self._AC6)*self._AC5/2**15
+            X2 = self._MC*2**11/(X1+self._MD)
+            self._B5 = X1+X2
+            self._t_B5 = pyb.millis()
+        return
 
-    # calculated temperature
-    def calc_temperature(self, uncomp_temperature):
+    # gauge pressure
+    def gauge_pressure(self):
         '''
-        Calculates and returns the compensted temperature.
+        Starts the pressure measurement and sets the time it will be
+        finished.
         '''
-        return ((self._B5(uncomp_temperature)+8)/2**4)/10
+        if self._B5 is None:
+            self._get_B5()
 
-    # calculated pressure
-    def calc_pressure(self, uncomp_temperature, uncomp_pressure):
+        delay = [5, 8, 14, 25]
+        self._bmp_i2c.mem_write(
+                                (0x34+(self.oversample_sett << 6)),
+                                self._bmp_addr,
+                                0xF4                                )
+
+        self._t_pressure_ready = pyb.millis() + delay[self.oversample_sett]
+        return
+
+    # get pressure
+    def get_pressure(self):
         '''
-        Calculates and returns the compensated pressure.
+        Waits until the pressure measurement is finished, then returns the
+        pressure.
         '''
-        B6 = self._B5(uncomp_temperature)-4000
-        X1 = (self.cal_B2*(B6*B6/2**12))/2**11
-        X2 = self.cal_AC2*B6/2**11
-        X3 = X1+X2
-        B3 = ((int((self.cal_AC1*4+X3)) << self.oversample_sett)+2)/4
-        X1 = self.cal_AC3*B6/2**13
-        X2 = (self.cal_B1*(B6*B6/2**12))/2**16
-        X3 = ((X1+X2)+2)/2**2
-        B4 = self.cal_AC4*(X3+32768)/2**15
-        B7 = (abs(uncomp_pressure)-B3) * (50000 >> self.oversample_sett)
-        if B7 < 0x80000000:
-            pressure = (B7*2)/B4
+        self._get_B5()
+        if self._t_pressure_ready is None:
+            self.gauge_pressure()
+        elif pyb.millis() <= self._t_pressure_ready:
+            self.get_pressure()
         else:
-            pressure = (B7/B4)*2
-        X1 = (pressure/2**8)**2
-        X1 = (X1*3038)/2**16
-        X2 = (-7357*pressure)/2**16
-        return pressure+(X1+X2+3791)/2**4
-
-    # temperature
-    def temperature(self):
-        '''
-        Measures and returns the compensated temperature.
-        '''
-        return self.calc_temperature(self.uncomp_temperature)
-
-    # pressure
-    def pressure(self):
-        '''
-        Measures and returns the compensated pressure.
-        '''
-        return self.calc_pressure(self.uncomp_temperature,
-                                  self.uncomp_pressure)
+            MSB = unp('<h', self._bmp_i2c.mem_read(1, self._bmp_addr, 0xF6))[0]
+            LSB = unp('<h', self._bmp_i2c.mem_read(1, self._bmp_addr, 0xF7))[0]
+            XLSB = unp('<h', self._bmp_i2c.mem_read(1, self._bmp_addr, 0xF8))[0]
+            UP = ((MSB << 16)+(LSB << 8)+XLSB) >> (8-self.oversample_sett)
+            B6 = self._B5-4000
+            X1 = (self._B2*(B6*B6/2**12))/2**11
+            X2 = self._AC2*B6/2**11
+            X3 = X1+X2
+            B3 = ((int((self._AC1*4+X3)) << self.oversample_sett)+2)/4
+            X1 = self._AC3*B6/2**13
+            X2 = (self._B1*(B6*B6/2**12))/2**16
+            X3 = ((X1+X2)+2)/2**2
+            B4 = self._AC4*(X3+32768)/2**15
+            B7 = (abs(UP)-B3) * (50000 >> self.oversample_sett)
+            if B7 < 0x80000000:
+                pressure = (B7*2)/B4
+            else:
+                pressure = (B7/B4)*2
+            X1 = (pressure/2**8)**2
+            X1 = (X1*3038)/2**16
+            X2 = (-7357*pressure)/2**16
+            self._t_pressure_ready = None
+            return pressure+(X1+X2+3791)/2**4
 
     # pressure baseline
     def baseline(self, dt=None):
@@ -196,25 +188,19 @@ class BMP180():
         sum_pressure = 0
         t_stop = pyb.millis()+dt
         while pyb.millis() < t_stop:
-            sum_pressure = sum_pressure + self.pressure()
+            sum_pressure = sum_pressure + self.get_pressure()
             count = count + 1
         return sum_pressure / count
 
     # altitude above reference
-    def altitude_above_ref(self, pressure, pressure_ref=None):
+    def altitude_above_baseline(self):
         '''
-        Calculates and returns the altitude relative to a reference pressure.
+        Calculates and returns the altitude relative to baseline.
 
-        | altitude |   pressure_ref |  
+        | altitude |       baseline |  
         |:--------:|:--------------:|  
-        | absolute |       baseline |  
+        | absolute |     baseline() |  
         | true     |        QNH*100 |  
         | pressure | 101325 or None |  
         '''
-        if pressure_ref == None:
-            pressure_ref = self.pressure_MSL
-        elif pressure_ref == 0:
-            print('pressure_ref can\'t be zero')
-            return None
-        else:
-            return 44330*(1-(pressure/pressure_ref)**(1/5.255))
+        return 44330*(1-(self.get_pressure()/self.baseline)**(1/5.255))
